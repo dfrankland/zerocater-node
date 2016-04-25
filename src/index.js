9519239af05cad826dd76690f3de5b38f92c7456
Dylan Frankland <github@frankland.io>
2016-04-25 14:08:36 -0700

    Add ability to format meal time

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import moment from 'moment';
 import rangeContainsDate from './rangeContainsDate';
 
 const Zerocater = token => ({
@@ -7,9 +8,11 @@ const Zerocater = token => ({
     const allMeals = await response.json();
     if (!options || !options.range) return allMeals;
     const range = options.range || false;
+    const timeFormat = options.timeFormat || false;
     return allMeals.reduce(
       (allFilteredMeals, nextMeal) => {
         const unixTime = nextMeal.time;
+        if (timeFormat) nextMeal.time = moment.unix(unixTime).format(timeFormat);
         if (!range) return allFilteredMeals.concat(nextMeal);
         if (rangeContainsDate(range, unixTime)) return allFilteredMeals.concat(nextMeal);
         return allFilteredMeals;
